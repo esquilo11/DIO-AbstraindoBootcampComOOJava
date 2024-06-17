f17b4ab853a6782c3e66cd3e142097250bc37b12
Andre <doria_95@hotmail.com>
2024-06-16 22:44:36 -0300

    mentoria e curso

---
+++ DIO-AbstraindoBootcampPOOJava/src/br/com/dio/desafio/dominio/Main.java
@@ -0,0 +1,32 @@
+package br.com.dio.desafio.dominio;
+
+import java.time.LocalDate;
+
+public class Main {
+    public static void main(String[] args) {
+        Curso curso1 = new Curso();
+        curso1.setTitulo("Curso Java");
+        curso1.setDescricao("Descrição curso java");
+        curso1.setCargaHoraria(8);
+
+        Curso curso2 = new Curso();
+        curso2.setTitulo("Curso Js");
+        curso2.setDescricao("Descrição curso Js");
+        curso2.setCargaHoraria(4);
+
+        Mentoria mentoria1 = new Mentoria();
+        mentoria1.setTitulo("Mentoria de Java");
+        mentoria1.setDescricao("Descrição mentoria de Java");
+        mentoria1.setData(LocalDate.now());
+
+        System.out.println(curso1);
+        System.out.println(curso2);
+
+
+
+
+
+
+
+    }
+}
